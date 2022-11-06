66346a79e725b0af3e3a4ab4bef57d3bea537b84
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-11-06 00:27:05 -0500

    Update Problem2a.py

--- Problem2a.py
+++ Problem2a.py
@@ -3,5 +3,9 @@
 # October 27, 2022
 # This program will print each of the numbers in the for loop on a new line
 
-for numbers in [4, 10, 13, 3, 6, 7, 42, 9, 20]:
-    print(numbers)
+#for numbers in [4, 10, 13, 3, 6, 7, 42, 9, 20]:
+#    print(numbers)
+#better to use a separate list
+lst = [4. 10, 13, 3, 6, 7, 42, 9, 20]
+for i in lst:
+    print(i)
