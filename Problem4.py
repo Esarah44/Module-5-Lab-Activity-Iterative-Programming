14926ee4fc73864f0a9e8196fa0d011ca77b01b5
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-11-06 00:37:19 -0500

    Update Problem4.py

--- Problem4.py
+++ Problem4.py
@@ -5,21 +5,27 @@
 
 import turtle
 
-polygon_sides = [int(input("Please enter the number of sides:"))]
+#polygon_sides = [int(input("Please enter the number of sides:"))]: no need to generate a list
+polygon_sides = int(input("Please enter the number of sides:"))
+
 length_side = int(input("Enter the length of the side:"))
 color_side = input("Enter the color of the line:")
 fill_polygon = input("Enter the fill color:")
 
 wn = turtle.Screen()
 alex = turtle.Turtle()
-
+alex.color(color_side, fill_polygon)
+ 
 alex.begin_fill()
 
-for i in range(polygon_sides[0]):
-    alex.color(color_side)
+#for i in range(polygon_sides[0]): : repeat the loop for number of sides
+for i in range(polygon_sides):
+   # alex.color(color_side): use it once when to use begin_fill, end_fill
     alex.forward(int(length_side))
-    alex.left(int(360) / int(polygon_sides[0]))
-    alex.fillcolor(fill_polygon)
+    #lex.left(int(360) / int(polygon_sides[0])): decide turning angle using sides
+    alex.left(360/polygon_sides) 
+   
+   # alex.fillcolor(fill_polygon):use it once when to use begin_fill, end_fill
 
 alex.end_fill()
 
